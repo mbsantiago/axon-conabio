744d584f55de09eb9d6e9b31c28e85509ecf6e8e
mbsantiago <santiago.mbal@gmail.com>
2018-09-25 14:33:54 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from distutils.core import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='axon-conabio',
-    version='0.1',
+    version='0.0.1',
     description='Conjunto de herramientas para crear, entrenar y evaluar modelos.', 
-    author='Santiago Mart√nez, Everardo Robredo',
+    author='Santiago Martinez, Everardo Robredo',
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
     author_email='santiago.mbal@gmail.com',
-    url='',
-    packages=['axon_conabio']
+    url='https://github.com/mbsantiago/axon-conabio',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
