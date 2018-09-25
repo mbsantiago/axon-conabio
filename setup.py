549bf3526b48405711dcf8196e958c9a77289e77
mbsantiago <santiago.mbal@gmail.com>
2018-09-25 17:04:28 -0500

    version control

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+version = {}
+with open("./axon_conabio/version.py") as fp:
+    exec(fp.read(), version)
+
 setup(
     name='axon-conabio',
-    version='0.0.1',
-    description='Conjunto de herramientas para crear, entrenar y evaluar modelos.', 
+    version=version,
+    description='Conjunto de herramientas para crear, entrenar y evaluar modelos.',
     author='Santiago Martinez, Everardo Robredo',
     long_description=long_description,
-    long_description_content_type="text/markdown", 
+    long_description_content_type="text/markdown",
     author_email='santiago.mbal@gmail.com',
     url='https://github.com/mbsantiago/axon-conabio',
     packages=find_packages(),
