14870cfba471e68814f1fe5351d0f133f8da09e9
mbsantiago <santiago.mbal@gmail.com>
2018-09-25 14:39:32 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
