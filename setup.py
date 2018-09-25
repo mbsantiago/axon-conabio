911a7214a7a3485291a7e0924fe76c3dc4529a60
mbsantiago <santiago.mbal@gmail.com>
2018-09-25 14:08:51 -0500

    primer commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='axon-conabio',
+    version='0.1',
+    description='Conjunto de herramientas para crear, entrenar y evaluar modelos.', 
+    author='Santiago Mart√nez, Everardo Robredo',
+    author_email='santiago.mbal@gmail.com',
+    url='',
+    packages=['axon_conabio']
+)
